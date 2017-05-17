53612ecdffef95b9492fe54b80ff5c3e0250517c
Kam Curacha <kamcuracha@yahoo.com>
2017-05-17 15:18:43 +0800

    Remove Titillium, Add Poppins. Footer change

--- wp-content/themes/lmtheme/footer.php
+++ wp-content/themes/lmtheme/footer.php
@@ -1,26 +1,60 @@
-<footer id="prefooter" class="section prefooter-section animatedParent">
+<footer id="prefooter" class="section prefooter-section">
   <div class="container">
-    <div class="footer-logo animated fadeInUpShort" data-id="1">
-      <a href="/"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-prime-urban.png" alt=""></a>
-    </div>
-    <div class="footer-social animated fadeInUpShort" data-id="2">
-      <?php if( have_rows('links', 'option') ): ?>
-          <?php while( have_rows('links', 'option') ): the_row(); ?>
-              <a target="_blank" href="<?php the_sub_field('link'); ?>"><i class="fa fa-<?php the_sub_field('media'); ?>" aria-hidden="true"></i></a>
-          <?php endwhile; ?>
-      <?php endif; ?>
-    </div>
-    <?php hybrid_get_menu( 'secondary' );  ?>
+      <div class="col-sm-3">
+          <div class="widget">
+              <h3 class="widget-title">Contact</h3>
+              <ul class="widget-address">
+                  <li>Southern Sun Financial Pty Ltd<br>
+                      14 Toorak Road, South Yarra 3141,<br>
+                      Melbourne Australia</li>
+                  <li>GPO Box 49, Melbourne VIC Australia</li>
+                  <li>Phone: 1800 234 567</li>
+                  <li>Fax: 1800 456 789</li>
+                  <li>ABN 62 081 162 843 </li>
+              </ul>
+          </div>
+      </div>
+      <div class="col-sm-3">
+          <div class="widget">
+              <h3 class="widget-title">About</h3>
+              <ul class="widget-links">
+                  <li><a href="/">Our Story</a></li>
+                  <li><a href="/">Our Team</a></li>
+                  <li><a href="/">Blog</a></li>
+                  <li><a href="/contact-us">Contact Us</a></li>
+              </ul>
+          </div>
+      </div>
+      <div class="col-sm-3">
+          <div class="widget">
+              <h3 class="widget-title">Legals</h3>
+              <ul class="widget-links">
+                  <li><a href="/">Privacy</a></li>
+                  <li><a href="/">Privacy Wealth Management</a></li>
+                  <li><a href="/">FAQ's</a></li>
+                  <li><a href="/">Terms & Conditions of Site</a></li>
+              </ul>
+          </div>
+      </div>
+      <div class="col-sm-3">
+          <div class="widget">
+              <h3 class="widget-title center">Apply in a few minutes</h3>
+              <p class="center">Lock in your repayments<br>
+                  Free no obligation quote<br>
+                  Speak to a finance professional
+                  <span class="highlight">For assistance call:</span>
+                  <span class="highlight2">1800 234 567</span>
+              </p>
+          </div>
+      </div>
   </div>
 </footer>
 
 <footer class="section footer-section">
   <div class="container">
-    <div class="t-footer-left">&copy Prime Urban <?php echo date("Y") ?>. All rights reserved.</div>
-    <div class="t-footer-right">Website by <a target="_blank" href="https://www.lightmedia.com.au/">Light Media</a></div>
+    <div class="footer-center">Copyright &copy 2016-2017 <?php echo get_bloginfo( 'name' ); ?>. All rights reserved. Website by <a target="_blank" href="https://www.lightmedia.com.au/">Light Media</a></div>
   </div>
 </footer>
-
 
 <?php wp_footer(); ?>
 </body>
